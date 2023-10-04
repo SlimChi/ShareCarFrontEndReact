4c0b295eb1dd672cebe5ecb16ab5a269fc6e827c
Marine <zimmer.marine@gmail.com>
2023-10-04 16:04:05 +0200

    [INIT][MZ] init project

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import "./app/assets/styles/index.css";
+import "react-toastify/dist/ReactToastify.css";
+
+import React from "react";
+import ReactDOM from 'react-dom/client'
+import { Provider } from "react-redux";
+
+import App from "./app/App";
+import { store } from "./app/redux-store/store";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
