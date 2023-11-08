2873ee1b2764697f64fc4289725e8feeb53d25aa
slimane <slimane.chihati>
2023-11-08 12:02:17 +0100

    ajout-image-voiture-back-front

--- src/app/components/Vehicle/CarouselVehicle.jsx
+++ src/app/components/Vehicle/CarouselVehicle.jsx
@@ -1,29 +1,54 @@
-import React from "react";
-import { Carousel } from "@material-tailwind/react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // Importez les styles du carrousel
 
 export default function CarouselVehicle() {
-    
+    const [imageURLs, setImageURLs] = useState([]);
 
+    useEffect(() => {
+        const fetchUserImages = async () => {
+            try {
+                const token = localStorage.getItem('token');
+                if (!token) {
+                    console.error("Le token d'authentification est manquant.");
+                    return;
+                }
+
+                const response = await axios.get('https://127.0.0.1:8000/api/get_voitures_images', {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json',
+                        'Authorization': 'Bearer ' + token,
+                    },
+                });
+
+                console.log("Réponse de la requête : ", response);
+
+                if (response.data.imagesVoiture && response.data.imagesVoiture.length > 0) {
+                    setImageURLs(response.data.imagesVoiture);
+                }
+            } catch (error) {
+                console.error("Erreur lors de la requête : ", error);
+            }
+        };
+
+        fetchUserImages();
+    }, []);
+
+    console.log("URLs des images : ", imageURLs);
 
     return (
-        <Carousel className="rounded-xl w-[30rem] h-[20rem]">
-        <img
-          src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
-          alt="image 1"
-          className="h-full w-full object-cover"
-        />
-        <img
-          src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-          alt="image 2"
-          className="h-full w-full object-cover"
-        />
-        <img
-          src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
-          alt="image 3"
-          className="h-full w-full object-cover"
-        />
-      </Carousel>
-   
+        <Carousel autoPlay showArrows infiniteLoop className="rounded-xl w-[30rem] h-[20rem]">
+            {imageURLs.map((imageURL, index) => (
+                <div key={index}>
+                    <img
+                        src={imageURL.url}
+                        alt={`image ${index + 1}`}
+                        className="h-full w-full object-cover"
+                    />
+                </div>
+            ))}
+        </Carousel>
     );
 }
-
